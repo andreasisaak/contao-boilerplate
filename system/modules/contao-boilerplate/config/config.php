8e382bcd9829707d22571281f248ebcd8f08ba5b
Andreas Isaak <andy.jared@googlemail.com>
2013-01-08 18:09:51 +0100

    Add localhost to blacklist

--- system/modules/contao-boilerplate/config/config.php
+++ system/modules/contao-boilerplate/config/config.php
@@ -62,5 +62,5 @@ $GLOBALS['TL_CONFIG']['default_js_minimizer']       = 'jsmin';
 $GLOBALS['TL_CONFIG']['theme_plus_lesscss_mode']    = '-';
 $GLOBALS['TL_CONFIG']['css_embed_images']           = 0;
 
-$GLOBALS['TL_CONFIG']['piwik_blacklist']            = 'a:2:{i:0;a:1:{s:3:"url";s:20:"http://192.168.19.42";}i:1;a:1:{s:3:"url";s:29:"http://preview.men-at-work.de";}}';
+$GLOBALS['TL_CONFIG']['piwik_blacklist']            = 'a:3:{i:0;a:1:{s:3:"url";s:16:"http://127.0.0.1";}i:1;a:1:{s:3:"url";s:20:"http://192.168.19.42";}i:2;a:1:{s:3:"url";s:29:"http://preview.men-at-work.de";}}';
 $GLOBALS['TL_CONFIG']['inactiveModules']            = 'a:6:{i:0;s:8:"calendar";i:1;s:8:"comments";i:2;s:3:"faq";i:3;s:10:"newsletter";i:4;s:4:"news";i:5;s:5:"tasks";}';
