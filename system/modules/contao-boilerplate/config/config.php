429f336fc6f297ed29a284163ec7706041006141
Andreas Isaak <andy.jared@googlemail.com>
2013-07-22 11:41:02 +0200

    Add new entries for the config.php

--- system/modules/contao-boilerplate/config/config.php
+++ system/modules/contao-boilerplate/config/config.php
@@ -1,29 +1,11 @@
-<?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
+<?php
 
 /**
  * Contao Open Source CMS
- * Copyright (C) 2005-2010 Leo Feyer
  *
- * Formerly known as TYPOlight Open Source CMS.
- *
- * This program is free software: you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation, either
- * version 3 of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this program. If not, please visit the Free
- * Software Foundation website at <http://www.gnu.org/licenses/>.
- *
- * PHP version 5
- * @copyright  Andreas Isaak
+ * @copyright  Andreas Isaak 2013 
  * @package    contao-boilerplate
- * @license    GNU/LGPL
+ * @license    GNU/LGPL 
  * @filesource
  *
  * @thanks     Tristan Lins <tristan.lins@infinitysoft.de>
@@ -41,7 +23,9 @@ $GLOBALS['TL_CONFIG']['timeZone']                   = 'Europe/Berlin';
 
 // URL settings
 $GLOBALS['TL_CONFIG']['rewriteURL']                 = true;
+$GLOBALS['TL_CONFIG']['urlSuffix']                  = '';
 $GLOBALS['TL_CONFIG']['folderAlias']                = true;
+$GLOBALS['TL_CONFIG']['folderUrl']                  = true;
 $GLOBALS['TL_CONFIG']['languageAlias']              = 'none';
 $GLOBALS['TL_CONFIG']['templateFiles']              = 'tpl,html5,xhtml,mobile';
 
@@ -54,6 +38,12 @@ $GLOBALS['TL_CONFIG']['imageHeight']                = 3000;
 $GLOBALS['TL_CONFIG']['gdMaxImgWidth']              = PHP_INT_MAX;
 $GLOBALS['TL_CONFIG']['gdMaxImgHeight']             = PHP_INT_MAX;
 $GLOBALS['TL_CONFIG']['jpgQuality']                 = 90;
+
+$GLOBALS['TL_CONFIG']['repository_unsafe_catalog']  = true;
+$GLOBALS['TL_CONFIG']['minifyMarkup']               = true;
+$GLOBALS['TL_CONFIG']['gzipScripts']                = true;
+$GLOBALS['TL_CONFIG']['bypassCache']                = true;
+$GLOBALS['TL_CONFIG']['doNotCollapse']              = true;
 
 // Theme+ settings
 $GLOBALS['TL_CONFIG']['default_compression']        = 'gzip';
