dbc380217eee81407e366471379744bd7fce450b
Andreas Isaak <andy.jared@googlemail.com>
2014-04-23 00:28:41 +0200

    Add support for the new contao 3.3 tinymce

--- 3.x/system/config/dcaconfig.php
+++ 3.x/system/config/dcaconfig.php
@@ -13,36 +13,50 @@
 // Display page ID
 $GLOBALS['TL_DCA']['tl_page']['list']['label']['fields'][] = 'id';
 $GLOBALS['TL_DCA']['tl_page']['list']['label']['format'] = '%s <span style="color: #aaaaaa; padding-left: 3px;">(ID: %s)</span>';
- 
+
 // Display article ID
 $GLOBALS['TL_DCA']['tl_article']['list']['label']['fields'][] = 'id'; 
 $GLOBALS['TL_DCA']['tl_article']['list']['label']['format'] = '%s <span style="color: #aaaaaa; padding-left: 3px;">(%s, ID: %s)</span>';
 
 // Narrow headlines
-$GLOBALS['TL_DCA']['tl_content']['fields']['headline']['options'] = array('h2', 'h3', 'h4', 'h5', 'h6', 'h1');
+$GLOBALS['TL_DCA']['tl_content']['fields']['headline']['options'] = array('h2', 'h3', 'h4', 'h5', 'h6');
 
 if (!\BackendUser::getInstance()->isAdmin)
 {
-    $GLOBALS['TL_DCA']['tl_content']['fields']['text']['eval']['rte'] = 'tinyMCE_boilerplate';
-    $GLOBALS['TL_DCA']['tl_news']['fields']['teaser']['eval']['rte'] = 'tinyMCE_boilerplate';
-    $GLOBALS['TL_DCA']['tl_news']['fields']['text']['eval']['rte'] = 'tinyMCE_boilerplate';
-    $GLOBALS['TL_DCA']['tl_calendar_events']['fields']['teaser']['eval']['rte'] = 'tinyMCE_boilerplate';
-    $GLOBALS['TL_DCA']['tl_calendar_events']['fields']['details']['eval']['rte'] = 'tinyMCE_boilerplate';
-    $GLOBALS['TL_DCA']['tl_newsletter']['fields']['teaser']['eval']['rte'] = 'tinyMCE_boilerplate';
-    $GLOBALS['TL_DCA']['tl_newsletter']['fields']['text']['eval']['rte'] = 'tinyMCE_boilerplate';
-    $GLOBALS['TL_DCA']['tl_faq']['fields']['answer']['eval']['rte'] = 'tinyMCE_boilerplate';
-    $GLOBALS['TL_DCA']['tl_form_field']['fields']['text']['eval']['rte'] = 'tinyMCE_boilerplate';
-    $GLOBALS['TL_DCA']['tl_comments']['fields']['comment']['eval']['rte'] = 'tinyMCE_boilerplate';
-    $GLOBALS['TL_DCA']['tl_article']['fields']['teaser']['eval']['rte'] = 'tinyMCE_boilerplate';
+    if (version_compare(VERSION, '3.2', '>')) {
+        $GLOBALS['TL_DCA']['tl_content']['fields']['text']['eval']['rte'] = 'tinyMCE_bp_3.3';
+        $GLOBALS['TL_DCA']['tl_news']['fields']['teaser']['eval']['rte'] = 'tinyMCE_bp_3.3';
+        $GLOBALS['TL_DCA']['tl_news']['fields']['text']['eval']['rte'] = 'tinyMCE_bp_3.3';
+        $GLOBALS['TL_DCA']['tl_calendar_events']['fields']['teaser']['eval']['rte'] = 'tinyMCE_bp_3.3';
+        $GLOBALS['TL_DCA']['tl_calendar_events']['fields']['details']['eval']['rte'] = 'tinyMCE_bp_3.3';
+        $GLOBALS['TL_DCA']['tl_newsletter']['fields']['teaser']['eval']['rte'] = 'tinyMCE_bp_3.3';
+        $GLOBALS['TL_DCA']['tl_newsletter']['fields']['text']['eval']['rte'] = 'tinyMCE_bp_3.3';
+        $GLOBALS['TL_DCA']['tl_faq']['fields']['answer']['eval']['rte'] = 'tinyMCE_bp_3.3';
+        $GLOBALS['TL_DCA']['tl_form_field']['fields']['text']['eval']['rte'] = 'tinyMCE_bp_3.3';
+        $GLOBALS['TL_DCA']['tl_comments']['fields']['comment']['eval']['rte'] = 'tinyMCE_bp_3.3';
+        $GLOBALS['TL_DCA']['tl_article']['fields']['teaser']['eval']['rte'] = 'tinyMCE_bp_3.3';
+    } else {
+        $GLOBALS['TL_DCA']['tl_content']['fields']['text']['eval']['rte'] = 'tinyMCE_bp_3.2';
+        $GLOBALS['TL_DCA']['tl_news']['fields']['teaser']['eval']['rte'] = 'tinyMCE_bp_3.2';
+        $GLOBALS['TL_DCA']['tl_news']['fields']['text']['eval']['rte'] = 'tinyMCE_bp_3.2';
+        $GLOBALS['TL_DCA']['tl_calendar_events']['fields']['teaser']['eval']['rte'] = 'tinyMCE_bp_3.2';
+        $GLOBALS['TL_DCA']['tl_calendar_events']['fields']['details']['eval']['rte'] = 'tinyMCE_bp_3.2';
+        $GLOBALS['TL_DCA']['tl_newsletter']['fields']['teaser']['eval']['rte'] = 'tinyMCE_bp_3.2';
+        $GLOBALS['TL_DCA']['tl_newsletter']['fields']['text']['eval']['rte'] = 'tinyMCE_bp_3.2';
+        $GLOBALS['TL_DCA']['tl_faq']['fields']['answer']['eval']['rte'] = 'tinyMCE_bp_3.2';
+        $GLOBALS['TL_DCA']['tl_form_field']['fields']['text']['eval']['rte'] = 'tinyMCE_bp_3.2';
+        $GLOBALS['TL_DCA']['tl_comments']['fields']['comment']['eval']['rte'] = 'tinyMCE_bp_3.2';
+        $GLOBALS['TL_DCA']['tl_article']['fields']['teaser']['eval']['rte'] = 'tinyMCE_bp_3.2';
+    }
 }
 
-/* if (!\BackendUser::getInstance()->isAdmin) {
+if (!\BackendUser::getInstance()->isAdmin) {
     foreach ((array) $GLOBALS['TL_HOOKS']['getSystemMessages'] as $k => $arrHook) { 
         if ($arrHook[1] == 'versionCheck') { 
             unset($GLOBALS['TL_HOOKS']['getSystemMessages'][$k]); 
         } 
     }
-} */
+}
 
 // Remove the space fields
 // unset($GLOBALS['TL_DCA']['tl_article']['fields']['space']);
