64b9f0f005aad77a20d1f4950fa7b0a11dc25835
andreasisaak <andy.jared@googlemail.com>
2014-06-12 12:23:25 +0200

    Change output_style, add development mode and optimize description

--- config.rb
+++ config.rb
@@ -8,12 +8,17 @@ images_dir = "files/standard/layout/images"
 javascripts_dir = "files/standard/layout/js"
 encoding = "UTF-8"
 
-# More infos
+# More infos about Contao and Sass/Compass
 # www.mediendepot-ruhr.de/sassscss-und-compass-als-css-preprocessor.html
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
-output_style = :expanded
+output_style = :compressed
+
+# Set specific rules only in development mode
+# How to use: 
+# https://gist.github.com/andreasisaak/814f251192828eb295b5
+environment = :development
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
@@ -23,9 +28,3 @@ line_comments = false
 
 # The cache_path is relative to the config.rb file and not the project_path or http_path.
 cache_path = 'system/tmp/.sass-cache'
-
-# If you prefer the indented syntax, you might want to regenerate this
-# project again passing --syntax sass, or you can uncomment this:
-# preferred_syntax = :sass
-# and then run:
-# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
